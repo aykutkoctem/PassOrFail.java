fa37c1b0b100604f5ceb871c9a2c572a2f466823
aykutkoctem <aykutkoctem@gmail.com>
2021-08-10 11:32:44 -0700

    day34

---
+++ src/day42_CustomClass_Constructors/ThisKeyword.java
@@ -0,0 +1,14 @@
+package day42_CustomClass_Constructors;
+
+public class ThisKeyword {
+    int a;
+    static  int b;
+    public void method1(){ System.out.println("Instance method"); }
+    public static void method2(){ System.out.println("Static method"); }
+
+    public ThisKeyword(){
+        System.out.println(this.a);
+        this.method1();
+       // System.out.println(this.b);
+    }
+}
---
+++ src/day42_CustomClass_Constructors/warmUpTask/CybertekStudentObjects.java
@@ -0,0 +1,15 @@
+package day42_CustomClass_Constructors.warmUpTask;
+
+public class CybertekStudentObjects {
+    public static void main(String[] args) {
+        CybertekStudent student1=new CybertekStudent("Feruza",20,'F');
+        CybertekStudent student2=new CybertekStudent("Alisa",27,'F');
+
+        System.out.println(student1);
+        System.out.println(student2);
+
+        student1.attendClass();
+        student2.replit();
+        student1.askForBreak();
+    }
+}
