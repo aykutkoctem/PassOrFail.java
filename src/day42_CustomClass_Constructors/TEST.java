3b3a92ef2220e3cfdbf4f59ec3d996ae50cfe428
aykutkoctem <aykutkoctem@gmail.com>
2021-08-12 08:04:27 -0700

    day43

---
+++ src/day42_CustomClass_Constructors/TEST.java
@@ -0,0 +1,14 @@
+package day42_CustomClass_Constructors;
+
+import java.util.Scanner;
+
+public class TEST {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+int [] arr={};
+
+
+
+    }
+
+}
---
+++ src/day43_OOP_Encapsulation/AccessModifiers.java
@@ -0,0 +1,11 @@
+package day43_OOP_Encapsulation;
+
+public class AccessModifiers {
+ public static int publicVariable=100;
+ //protected static int protectVariable=1000;
+ static int defaultVariable=200; // access modifier:default
+ private static int privateVariable=200;
+
+
+
+}
