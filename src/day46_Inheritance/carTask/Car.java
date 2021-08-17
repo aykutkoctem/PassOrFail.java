e433e9bdfd521d6b923bc3e1db4188decc50bcbe
aykutkoctem <aykutkoctem@gmail.com>
2021-08-17 07:41:22 -0700

    day43

---
+++ src/day46_Inheritance/carTask/Car.java
@@ -0,0 +1,44 @@
+package day46_Inheritance.carTask;
+
+public class Car {
+public String brand,model;
+public final int year;
+public double price;
+public String color;
+public long miles;
+
+    public Car(String brand, String model, int year, double price, String color, long miles) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+        this.miles = miles;
+    }
+
+    public void start(){
+        System.out.println(brand+" "+model+" is starting.");
+    }
+    public void drive(){
+        System.out.println(brand+" "+model+" is ready to drive.");
+    }
+
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                ", miles=" + miles +
+                '}';
+    }
+}
+/*
+Create a class called Car
+			instance variables:
+				brand, model, year, price, color, miles
+				add a constructor to set all the fields
+			instance methods:
+				start(), drive(),toString()
+ */
---
+++ src/day46_Inheritance/carTask/Toyota.java
@@ -0,0 +1,14 @@
+package day46_Inheritance.carTask;
+
+public class Toyota extends Car {
+    public static boolean isReliable = true;
+
+    static {
+        isReliable=true;
+    }
+    public Toyota(String brand, String model, int year, double price, String color, long miles) {
+
+        super(brand, model, year, price, color, miles);
+
+    }
+}
